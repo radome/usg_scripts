74cfb701d87d3a5f68c9b6d9db73eb397a1d69be
Neil <nts@sanger.ac.uk>
2014-01-15 12:13:27 +0000

    validate samples prior to ena accessioning

--- Scripts/validate_ena_samples.rb
+++ Scripts/validate_ena_samples.rb
@@ -1,3 +1,4 @@
+sample_ids =[]
 samples.each do |sample_name|
   sample = Sample.find_by_name(sample_name)
   begin
@@ -5,5 +6,6 @@ samples.each do |sample_name|
     puts "#{sample.id},#{sample.name}"
   rescue ActiveRecord::RecordInvalid => invalid
     puts "#{sample.id},#{sample.name},#{invalid}"
+    sample_ids << sample.id
   end
 end; nil
