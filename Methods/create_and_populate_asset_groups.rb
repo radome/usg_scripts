e22d84d0f860e34225de87bfb3876010fb7b3299
Neil <nts@sanger.ac.uk>
2014-10-14 13:15:44 +0100

    create and populate asset group for sample tubes)

---
+++ Methods/create_and_populate_asset_groups.rb
@@ -0,0 +1,24 @@
+# This is for sample tubes only not wells
+def create_and_populate_asset_groups(sample_group_hash, study_id, mode)
+  ActiveRecord::Base.transaction do
+    puts "Creating #{sample_group_hash.first[1]}"
+    @ag = AssetGroup.create!(:name => sample_group_hash.first[1], :study_id => study_id)
+    sample_group_hash.each do |sample_name,ag_name|
+      sample_tube = Sample.find_by_name(sample_name).assets.first
+      if ag_name == @ag.name
+        @ag.assets << sample_tube
+        @ag.save!
+        puts "#{@ag.assets.map(&:name).inspect}"
+      else
+        puts "http://production.psd.sanger.ac.uk:6600/studies/#{study_id}/asset_groups/#{@ag.id}"
+        puts "Creating #{ag_name}"
+        @ag = AssetGroup.create!(:name => ag_name, :study_id => study_id)
+        @ag.assets << sample_tube
+        @ag.save!
+        puts "#{@ag.assets.map(&:name).inspect}"
+      end
+    end; nil
+    puts "http://production.psd.sanger.ac.uk:6600/studies/#{study_id}/asset_groups/#{@ag.id}"
+    raise "Hell!!!!" unless mode == 'run'
+  end
+end
