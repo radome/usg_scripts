b904ec6c241318df6df2b0464f2c522eba4c0764
Neil <nts@sanger.ac.uk>
2014-10-17 11:53:29 +0100

    updated request types

--- Methods/cancel_submission.rb
+++ Methods/cancel_submission.rb
@@ -1,22 +1,47 @@
-def cancel_submission(submission_ids,mode)
+def cancel_submission(subs,rt_ticket,login,mode)
   ActiveRecord::Base.transaction do
-    submission_ids.each do |submission_id|
-      sub = Submission.find submission_id
-      sub.requests.each do |r| 
-        status = r.state
-        if r.state == "passed"
-          # r.cancel_completed!
-          r.return!
-          puts "#{sub.id} request #{r.id} #{r.sti_type} was #{status} now => #{r.state}"
-        elsif r.state == "pending"
-          r.cancel_before_started!
-          puts "#{sub.id} request #{r.id} #{r.sti_type} was #{status} now => #{r.state}"
+    comment = "#{Time.now}: Submission cancelled by #{login} via RT ticket #{rt_ticket}"
+    odd_classes = ["IlluminaHtp::Requests::CherrypickedToShear","IlluminaHtp::Requests::PostShearToAlLibs","IlluminaHtp::Requests::PrePcrToPcr","IlluminaHtp::Requests::PcrXpToPool"]
+    subs.each do |submission_name|
+      submission = Submission.find_by_name submission_name
+      requests = submission.requests
+      requests.each do |r|
+        puts "#{r.inspect}"
+        if r.class == TransferRequest
+          puts "Ignore..."
         else
-          puts "#{sub.id} request #{r.id} #{r.sti_type} was #{status} now => #{r.state}"
+          status = r.state
+          if r.state == "passed"
+            if odd_classes.include?(r.class.name)
+              r.fail!
+              # r.cancel_completed!
+              puts "#{submission.id} request #{r.id} #{r.sti_type} was #{status} now => #{r.state}"
+            else
+              r.change_decision!
+              # r.return!
+              puts "#{submission.id} request #{r.id} #{r.sti_type} was #{status} now => #{r.state}"
+            end
+          elsif r.state == "pending"
+            r.cancel_before_started!
+            puts "#{submission.id} request #{r.id} #{r.sti_type} was #{status} now => #{r.state}"
+          else
+            puts "#{submission.id} request #{r.id} #{r.sti_type} was #{status} now => #{r.state}"
+          end
+          # r.destroy
         end
-        r.update_attributes!(:asset => nil)
       end
+      puts "#{Submission.find(submission.id).requests.map(&:state).uniq.inspect}"
+    end
+    submission.orders.each do |o|
+      puts "#{comment}"
+      o.comments = comment
+      o.save!
     end
     raise "Hell!!" unless mode == "run"
   end; nil
 end
+rt_ticket = 416118
+login = 'nts'
+mode = 'test'
+subs = ["1_6740_3135_08072014"]
+cancel_submission(subs,rt_ticket,login,mode)
