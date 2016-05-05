6be7d70603cf9f190e30a21256ced862d6c798ac
Neil <nts@sanger.ac.uk>
2016-05-05 12:09:36 +0100

    updates to code

--- Methods/update_named_samples_accession_in_study.rb
+++ Methods/update_named_samples_accession_in_study.rb
@@ -3,11 +3,17 @@ class ::Sample
   def accession_service; @acession_service; end
 end
 
+def update_sample(name)
+  ActiveRecord::Base.transaction do
+    sample = Sample.find_by_name(name)
+    sample = Sample.find_by_id(name) if sample == nil
+    # sample.sample_metadata.update_attributes!(:sample_taxon_id => 9606, :sample_common_name => 'Homo Sapien')
+    # sample.sample_metadata.update_attributes!(:phenotype => 'Not applicable')
+    return sample
+  end
+end
+
 def update_sample_accession_by_study(sample_names,study_id,login)
-  # common_taxon_hash = {
-  #   "Klebsiella pneumoniae" => 573,
-  #   "Escherichia coli" => 562
-  # }
   user = User.find_by_login(login)
   study = Study.find(study_id)
   puts "#{study.name}"
@@ -19,48 +25,51 @@ def update_sample_accession_by_study(sample_names,study_id,login)
     c = sample_names.size
     # taxon_id = 1070528
     sample_names.each do |name|
-      ActiveRecord::Base.transaction do
-        sample = Sample.find_by_name(name)
-        sample = Sample.find_by_id(name) if sample == nil
-        # sample.sample_metadata.update_attributes!(:sample_taxon_id => 9606, :sample_common_name => 'Homo Sapien')
-        # sample.sample_metadata.update_attributes!(:phenotype => 'Not applicable', :gender => 'unknown')
-        x = nil
-        a = []
+      sample = update_sample(name)
+      x = nil
+      a = []
+      begin
+        sample.validate_ena_required_fields!
+      rescue ActiveRecord::RecordInvalid => invalid
+        x = invalid.record.errors
+        a << "#{sample.id}, #{sample.name}, #{sample.sample_metadata.sample_ebi_accession_number}, #{invalid.record.errors[:base].uniq.join(', ')}"
+        puts "XXX #{a}"
+      end
+      sample_errors << a
+      
+      if x.nil? # and sample.sample_metadata.sample_ebi_accession_number.nil?
+        puts "#{c} *** #{sample.name} ***"
         begin
-          sample.validate_ena_required_fields!
-        rescue ActiveRecord::RecordInvalid => invalid
-          x = invalid.record.errors
-          a << "#{sample.id}, #{sample.name}, #{sample.sample_metadata.sample_ebi_accession_number}, #{invalid.record.errors[:base].uniq.join(', ')}"
-          puts "XXX #{a}"
+          sample.accession_service = study.accession_service
+          study.accession_service.submit(
+          user,
+          Accessionable::Sample.new(sample)
+          )
+        rescue AccessionService::AccessionServiceError => invalid
+          duds << invalid.message
+          puts invalid.message        
         end
-        sample_errors << a
-        if x.nil? # and sample.sample_metadata.sample_ebi_accession_number.nil?
-          puts "#{c} *** #{sample.name} ***"
-          begin
-            sample.accession_service = study.accession_service
-            study.accession_service.submit(
-            user,
-            Accessionable::Sample.new(sample)
-            )
-          rescue AccessionService::AccessionServiceError => invalid
-            duds << invalid.message
-            puts invalid.message        
-          end
-        else
-          puts "#{c} Ignoring #{sample.name} <<<<<<<<<<<<<<<<<<"
-        end
+      else
+        puts "#{c} Ignoring #{sample.name} <<<<<<<<<<<<<<<<<<"
       end
       c -=1
     end; nil
     sample_errors = sample_errors.flatten; nil
     puts "#{study.name}"
-    puts "Sample errors #{sample_errors.size}"
 
-    sample_errors.each do |sample_error|
-      puts "#{sample_error.inspect}\n"
+    unless sample_errors.nil?
+      puts "Sample validation errors #{sample_errors.size}"
+      sample_errors.each do |sample_error|
+        puts "#{sample_error.inspect}"
+      end; nil
+    end
+    
+    unless duds.nil?
+      puts "Errors from EBI: #{duds.size}"
+      duds.each {|d| puts "#{d.inspect}\n"}
     end; nil
+    
   end
 end
-
 
 # update_sample_accession_by_study(sample_names,study_id,login)
--- Methods/update_sample_accession_by_study.rb
+++ Methods/update_sample_accession_by_study.rb
@@ -3,6 +3,14 @@ class ::Sample
   def accession_service; @acession_service; end
 end
 
+def update_sample(sample)
+  ActiveRecord::Base.transaction do
+    # sample.sample_metadata.update_attributes!(:sample_taxon_id => 9606, :sample_common_name => 'Homo sapiens')
+    # sample.sample_metadata.update_attributes!(:phenotype => 'Not applicable')
+    return sample
+  end
+end
+
 def update_sample_accession_by_study(study_ids,login)
   user = User.find_by_login(login)
   Study.find(study_ids).each do |study|
@@ -14,51 +22,51 @@ def update_sample_accession_by_study(study_ids,login)
       sample_errors = []
       c = study.samples.size
       study.samples.find_each do |sample|
-        ActiveRecord::Base.transaction do
-          # Add missing metadata here
-          # sample.sample_metadata.update_attributes!(:gender => 'Unknown')
-          # sample.sample_metadata.update_attributes!(:sample_taxon_id => 9606) unless sample.sample_metadata.sample_taxon_id !=nil
-          # sample.sample_metadata.update_attributes!(:sample_common_name => "Homo sapiens") unless sample.sample_metadata.sample_common_name !=nil
-          x = nil
-          a = []
-          begin
-            sample.validate_ena_required_fields!
-          rescue ActiveRecord::RecordInvalid => invalid
-            x = invalid.record.errors
-            a << "#{sample.id}, #{sample.name}, #{sample.sample_metadata.sample_ebi_accession_number}, #{invalid.record.errors[:base].uniq.join(', ')}"
-            puts "#{a}"
-          end
-          sample_errors << a
-          if x.nil? # and sample.sample_metadata.sample_ebi_accession_number.nil?
-            puts "#{c} *** #{sample.name} ***"
-            begin
-              sample.accession_service = study.accession_service
-              study.accession_service.submit(
-              user,
-              Accessionable::Sample.new(sample)
-              )
-            rescue AccessionService::AccessionServiceError => invalid
-              duds << invalid.message
-              puts invalid.message
-            end
-          else
-            puts "#{c} Ignoring #{sample.name} <<<<<<<<<<<<<<<<<<"
-          end
+        sample = update_sample(sample)
+        x = nil
+        a = []
+        begin
+          sample.validate_ena_required_fields!
+        rescue ActiveRecord::RecordInvalid => invalid
+          x = invalid.record.errors
+          a << "#{sample.id}, #{sample.name}, #{sample.sample_metadata.sample_ebi_accession_number}, #{invalid.record.errors[:base].uniq.join(', ')}"
+          puts "#{a}"
         end
-        c -=1
-      end; nil
-      sample_errors = sample_errors.flatten; nil
-      puts "#{study.name}"
-      puts "Sample errors #{sample_errors.size}"
-  
+        sample_errors << a
+        
+        if x.nil? # and sample.sample_metadata.sample_ebi_accession_number.nil?
+          puts "#{c} *** #{sample.name} ***"
+          begin
+            sample.accession_service = study.accession_service
+            study.accession_service.submit(
+            user,
+            Accessionable::Sample.new(sample)
+            )
+          rescue AccessionService::AccessionServiceError => invalid
+            duds << invalid.message
+            puts invalid.message
+          end
+        else
+          puts "#{c} Ignoring #{sample.name} <<<<<<<<<<<<<<<<<<"
+        end
+      end
+      c -=1
+    end; nil
+    sample_errors = sample_errors.flatten; nil
+    puts "#{study.name}"
+    
+    unless sample_errors.nil?
+      puts "Sample validation errors #{sample_errors.size}"
       sample_errors.each do |sample_error|
         puts "#{sample_error.inspect}"
-      end
-      unless duds.nil?
-        puts "Duds>>"
-        duds.map {|d| puts "#{d}\n"}; nil
-      end
+      end; nil
     end
+    
+    unless duds.nil?
+      puts "Errors from EBI: #{duds.size}"
+      duds.each {|d| puts "#{d.inspect}\n"}
+    end; nil
+    
   end; nil
 end
 
