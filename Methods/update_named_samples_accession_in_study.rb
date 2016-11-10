c51b8e8092a801a09150e47cb8f0ec4446b4390e
Neil <nts@sanger.ac.uk>
2016-11-10 15:45:40 +0000

    make class distinction between sample names and ids

--- Methods/update_named_samples_accession_in_study.rb
+++ Methods/update_named_samples_accession_in_study.rb
@@ -1,43 +1,59 @@
+require 'builder'
+
 class ::Sample
   def accession_service=(as); @acession_service = as; end
   def accession_service; @acession_service; end
 end
 
-def update_sample(name)
-  ActiveRecord::Base.transaction do
-    sample = Sample.find_by_name(name)
-    sample = Sample.find_by_id(name) if sample == nil
-    # sample.sample_metadata.update_attributes!(:sample_taxon_id => 9606, :sample_common_name => 'Homo Sapien')
-    # sample.sample_metadata.update_attributes!(:phenotype => 'Not applicable')
-    return sample
-  end
+def update_sample(sample_name)
+  sample = Sample.find_by_name(sample_name) if sample_name.class == String
+  sample = Sample.find_by_id(sample_name) if sample_name.class == Fixnum
+  # Update metadata here
+  # sample.sample_metadata.sample_taxon_id = 9606
+#   sample.sample_metadata.sample_common_name = 'Homo sapiens'
+    # sample.sample_metadata.gender = 'unknown'
+  # sample.sample_metadata.donor_id = sample.name
+#   if sample.sample_metadata.phenotype.nil?
+#     sample.sample_metadata.phenotype = 'Not provided'
+#   end
+  # if sample.sample_metadata.sample_ebi_accession_number != nil && sample.sample_metadata.sample_ebi_accession_number.match(/ERS/)
+  #   sample.sample_metadata.sample_ebi_accession_number = nil
+  # end
+  return sample
 end
 
-def update_sample_accession_by_study(sample_names,study_id,login)
+def update_sample_accession_by_sample(sample_names,study_id,login)
   user = User.find_by_login(login)
   study = Study.find(study_id)
   puts "#{study.name}"
-  if study.study_metadata.study_ebi_accession_number.match(/EGA/).nil? == false && study.study_metadata.data_release_strategy == 'open'
+  if study.study_metadata.study_ebi_accession_number.nil?
+    raise "study #{study.id} requires accessioning"
+  elsif study.study_metadata.study_ebi_accession_number.match(/EGA/).nil? == false && study.study_metadata.data_release_strategy == 'open'
     puts "Stopping!\nStudy is set to open yet has an EGA accession #{study.study_metadata.study_ebi_accession_number}: This will result in samples being given ENA accessions"
   else
     sample_errors = []
     duds = []
     c = sample_names.size
-    # taxon_id = 1070528
-    sample_names.each do |name|
-      sample = update_sample(name)
+    sample_names.each do |sample_name|     
+      sample = update_sample(sample_name)
       x = nil
       a = []
       begin
         sample.validate_ena_required_fields!
       rescue ActiveRecord::RecordInvalid => invalid
         x = invalid.record.errors
-        a << "#{sample.id}, #{sample.name}, #{sample.sample_metadata.sample_ebi_accession_number}, #{invalid.record.errors[:base].uniq.join(', ')}"
-        puts "XXX #{a}"
+        # puts "#{x.inspect}"
+        if invalid.record.errors[:base].uniq.first == "Study metadata study study title can't be blank on study"
+          x = nil
+        else
+          a << "#{sample.id}, #{sample.name}, #{sample.sample_metadata.sample_ebi_accession_number}, #{invalid.record.errors[:base].uniq.join(', ')}"
+          puts "#{a}"
+          sample_errors << a
+        end
       end
-      sample_errors << a
-      
-      if x.nil? # and sample.sample_metadata.sample_ebi_accession_number.nil?
+
+
+      if x.nil?
         puts "#{c} *** #{sample.name} ***"
         begin
           sample.accession_service = study.accession_service
@@ -46,14 +62,17 @@ def update_sample_accession_by_study(sample_names,study_id,login)
           Accessionable::Sample.new(sample)
           )
         rescue AccessionService::AccessionServiceError => invalid
-          duds << invalid.message
-          puts invalid.message        
+          message = "#{sample.id}, #{sample.name}, #{sample.sample_metadata.sample_common_name} "+ invalid.message
+          duds << message
+          puts message
         end
+        sample.save!
       else
         puts "#{c} Ignoring #{sample.name} <<<<<<<<<<<<<<<<<<"
       end
       c -=1
     end; nil
+  
     sample_errors = sample_errors.flatten; nil
     puts "#{study.name}"
 
@@ -63,13 +82,10 @@ def update_sample_accession_by_study(sample_names,study_id,login)
         puts "#{sample_error.inspect}"
       end; nil
     end
-    
+
     unless duds.nil?
       puts "Errors from EBI: #{duds.size}"
       duds.each {|d| puts "#{d.inspect}\n"}
     end; nil
-    
   end
 end
-
-# update_sample_accession_by_study(sample_names,study_id,login)
