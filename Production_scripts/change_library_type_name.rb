cfec977de5765d13f717e4c23ca57c0525e4af3d
Neil <nts@sanger.ac.uk>
2019-04-18 12:27:00 +0100

    Use to update submission introduced errors in library name

--- Production_scripts/change_library_type_name.rb
+++ Production_scripts/change_library_type_name.rb
@@ -7,3 +7,5 @@ def change_library_type_name(lane_ids, lib_type_name = "Chromium single cell")
     batches.map {|b| b.touch}; nil
   end
 end
+
+# select DISTINCT(entity_id_lims) from iseq_flowcell where BINARY pipeline_id_lims LIKE '%chromium single cell%';
