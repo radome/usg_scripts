9ba9a669baa4f78f8cdbe1b74ae7404fc35aa891
Neil <nts@sanger.ac.uk>
2019-07-16 14:39:01 +0100

    Cancel submission made in error but started

---
+++ Production_scripts/new_cancel_submission.rb
@@ -0,0 +1,32 @@
+def find_submission(id)
+  if id.is_a?(Integer)
+    sub = Submission.find_by(id: id)
+  else
+    sub = Submission.find_by(name: id)
+  end
+  return sub
+end
+
+def new_cancel_submission(sub_identifiers)
+  ActiveRecord::Base.transaction do
+    sub_identifiers.each do |id|
+      sub = find_submission(id)
+      sub.requests.each do |req|
+        if req.state == 'pending'
+          req.cancel_before_started!
+        else
+          req.cancel! unless req.state == 'cancelled'
+        end
+      end
+      TransferRequest.where(submission_id: sub.id).map(&:cancel!); nil
+      sub.reload
+      if sub.requests.map(&:state).uniq == ['cancelled']
+        sub.state = 'cancelled'; sub.save!
+        puts "sub #{id} #{sub.state}"
+      else
+        puts "sub #{id} #{sub.requests.map(&:state).uniq}"
+      end
+    end
+  end
+end
+
